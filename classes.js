39631b4073ec46f77cd70d006797f7f4fbb22756
SofiaGrigoriadou <sofiagrigoria@proton.me>
2026-06-24 17:35:16 +0300

    Update classes.js

--- classes.js
+++ classes.js
@@ -2,4 +2,20 @@ let player = {
   strength: 1,
   intelligence: 1,
   charisma: 1
+
+  document
+  .getElementById("str")
+  .textContent =
+  player.strength;
+
+  document
+  .getElementById("int")
+  .textContent =
+  player.intelligence;
+
+  document
+  .getElementById("cha")
+  .textContent =
+  player.charisma;
 };
+
