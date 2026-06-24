73e5631ff3bac5a880ada622df7c4f8642c23d54
SofiaGrigoriadou <sofiagrigoria@proton.me>
2026-06-24 18:36:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,132 +1,79 @@
-let xp =
-Number(localStorage.getItem("xp")) || 0;
+let xp = Number(localStorage.getItem("xp")) || 0;
 
-let level =
-Number(localStorage.getItem("level")) || 1;
+let level = Number(localStorage.getItem("level")) || 1;
 
-let player =
-JSON.parse(
-localStorage.getItem("player")
-) || {
-strength: 1,
-intelligence: 1,
-charisma: 1
+let player = JSON.parse(localStorage.getItem("player")) || {
+    strength: 1,
+    intelligence: 1,
+    charisma: 1
 };
 
 function saveGame() {
-
-```
-localStorage.setItem(
-    "xp",
-    xp
-);
-
-localStorage.setItem(
-    "level",
-    level
-);
-
-localStorage.setItem(
-    "player",
-    JSON.stringify(player)
-);
-```
-
+    localStorage.setItem("xp", xp);
+    localStorage.setItem("level", level);
+    localStorage.setItem("player", JSON.stringify(player));
 }
 
 function updateStats() {
-
-```
-document
-    .getElementById("xp")
-    .textContent = xp;
-
-document
-    .getElementById("level")
-    .textContent = level;
-
-document
-    .getElementById("str")
-    .textContent =
-    player.strength;
-```
-
+    document.getElementById("xp").textContent = xp;
+    document.getElementById("level").textContent = level;
+    
+    document.getElementById("str").textContent =player.strength;
+    document.getElementById("int").textContent =player.intelligence;
+    document.getElementById("cha").textContent =player.charisma;
 }
 
 function checkLevelUp() {
-
-```
-while (xp >= level * 100) {
-
-    xp -= level * 100;
-
-    level++;
-
-    alert(
-        "Level Up! You are now level "
-        + level
-    );
-
-}
-```
-
+    while (xp >= level * 100) {
+        xp -= level * 100;
+        level++;
+        alert(
+        "Level Up! You are now level " + level);
+    }
 }
 
 function addTask() {
-
-```
-const input =
-    document.getElementById(
-        "taskInput"
-    );
-
-const taskText =
-    input.value.trim();
-
-if (taskText === "") {
-    return;
-}
-
-const li =
-    document.createElement("li");
-
-li.textContent =
-    taskText + " ";
-
-const button =
-    document.createElement(
-        "button"
-    );
-
-button.textContent =
-    "Complete";
-
-button.onclick =
-    function () {
+    const input = document.getElementById("taskInput");
+    const type = document.getElementById("taskType").value;
+    
+    const text = input.value.trim();
+    if (!text) return;
+    
+    const li = document.createElement("li");
+    
+    li.textContent = taskText + " ";
+    
+    const btn = document.createElement("button");
+    btn.textContent = "Complete";
+    
+    btn.onclick = function () {
 
         xp += 10;
 
-        player.strength += 1;
+       if (type === "strength") {
+      player.strength += 1;
+    }
+
+    if (type === "intelligence") {
+      player.intelligence += 1;
+    }
+
+    if (type === "charisma") {
+      player.charisma += 1;
+    }
 
         checkLevelUp();
-
         updateStats();
-
         saveGame();
 
         li.remove();
 
     };
 
-li.appendChild(button);
-
-document
-    .getElementById("taskList")
-    .appendChild(li);
+li.appendChild(btn);
+document.getElementById("taskList").appendChild(li);
 
 input.value = "";
-```
-
 }
 
 updateStats();
